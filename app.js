665a8bdf17c3363af34a592ab37592c8f6c1bbf6
keisukehonda <keisukeh@gmail.com>
2015-07-21 00:45:07 +0900

    add admin page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var register = require('./routes/register');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/register', register);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
