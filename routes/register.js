da561648f14ba94d61b439cac82d92d99a4a83b0
keisukehonda <keisukeh@gmail.com>
2015-07-21 01:22:22 +0900

    add Title, etc.

--- routes/register.js
+++ routes/register.js
@@ -4,13 +4,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('register', { title: 'Express' });
+    res.render('register', { title: 'Register' });
 });
 
 /* POST method */
 router.post('/', function(req, res) {
     console.log(req.body.name);
-    res.render('register/confirm', {title: 'confirm', name: req.body.name});
+    res.render('register/confirm', {title: 'Register', name: req.body.name});
 });
 
 module.exports = router;
