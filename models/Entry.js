ba6d4fa81ac215f166acd329e101aa94ab8952af
keisukehonda <keisukeh@gmail.com>
2015-07-27 11:45:37 +0900

    add validator

--- models/Entry.js
+++ models/Entry.js
@@ -1,10 +1,16 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
 
 mongoose.connect('mongodb://localhost/register_app');
 
 var schema = new mongoose.Schema({
-    name: String,
-    mail: String
+    firstname: {type: String, required:'First Name is required'},
+    lastname: {type: String, required:'Last Name is required'},
+    Title: {type: String, required:'Title is is required'},
+    email: {type: String, required:'Email is required'},
+    organization: {type: String, required:'Organization is required'},
+    country: {type: String, required: 'Country is required'}
 });
 
 module.exports = mongoose.model('Entry', schema);
--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var entry = require('../models/Entry');
+var Entry = require('../models/Entry');
 var router = express.Router();
 
 /* GET home page. */
@@ -10,16 +10,26 @@ router.get('/', function(req, res, next) {
 /* POST method */
 router.post('/', function(req, res) {
     console.log(req.body.email);
-    res.render('register/confirm',
-               {title: 'Registration for HPCCOM 2015',
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                Title: req.body.Title,
-                email: req.body.email,
-                organization: req.body.organization,
-                country: req.body.country
-               }
-              );
+
+    //validation
+    var newEntry = new Entry(req.body);
+
+    newEntry.save(function(err) {
+        if(err) {
+            console.log(err.errors);
+            //retry
+            res.redirect('/register');
+        }else{
+            res.render('register/confirm',
+                       {title: 'Registration for HPCCOM 2015',
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        Title: req.body.Title,
+                        email: req.body.email,
+                        organization: req.body.organization,
+                        country: req.body.country});
+        }
+    });
 });
 
 module.exports = router;
